1ee0c3fb3f9706ae9ca8f78f78a8bc8975d80418
Amir Mohsen Abdolrazaghi <amirmohsen.a@gmail.com>
2019-11-11 00:38:43 +0000

    isSameAs unit test

--- src/fp/currency/isSameAs/index.ts
+++ src/fp/currency/isSameAs/index.ts
@@ -1,8 +1,13 @@
-import isEqual from 'lodash/isEqual';
 import { FrozenBaseCurrency } from 'src/fp/types';
 import isCurrency from '../isCurrency';
 
+const isCodeAndDecimalDigitsTheSame = (currencyA: FrozenBaseCurrency, currencyB: FrozenBaseCurrency): boolean => {
+  const { code: codeA, decimalDigits: decimalDigitsA } = currencyA;
+  const { code: codeB, decimalDigits: decimalDigitsB } = currencyB;
+  return codeA === codeB && decimalDigitsA === decimalDigitsB;
+};
+
 const isSameAs = (currencyA: FrozenBaseCurrency, currencyB: FrozenBaseCurrency): boolean =>
-  isCurrency(currencyA) && isCurrency(currencyB) && isEqual(currencyA, currencyB);
+  isCurrency(currencyA) && isCurrency(currencyB) && isCodeAndDecimalDigitsTheSame(currencyA, currencyB);
 
 export default isSameAs;
